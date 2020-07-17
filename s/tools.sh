6810c73c4d39b99af5c66dcfd821c5410a55e38d
awada <zhengyao365@163.com>
2020-07-17 13:13:08 +0800

    separate tools

--- s/tools.sh
+++ s/tools.sh
@@ -3,13 +3,45 @@
 
 common="nload psmisc net-tools nethogs openssh-server openssh-clients cronie"
 
+if ! [ -x "$(command -v sudo)" ]; then
+    apt-get update -y
+    yum update -y
+
+    apt-get install -y sudo
+    yum install -y sudo 
+fi;
+
 if [ -x "$(command -v apt)" ]; then
+    sudo apt-get install software-properties-common
     sudo apt-get install -y $common
 fi;
 
 if [ -x "$(command -v yum)" ]; then
     sudo yum install -y epel-release
     sudo yum install -y $common
+fi;
+
+if ! [ -x "$(command -v n)" ]; then
+    common="curl screen npm"
+
+    sudo apt-get install -y software-properties-common 
+    sudo yum install -y epel-release 
+
+    sudo apt-get install -y $common
+    sudo yum install -y $common
+    
+    npm config set prefix $HOME/.npm_global
+    npm config set registry https://registry.npm.taobao.org/
+    npm i -g n
+    n latest
+    PATH="$PATH"
+    
+    npm i -g nodemon 
+    npm i -g typescript 
+    npm i -g trash-cli
+    npm i -g yarn 
+    yarn config set registry https://registry.npm.taobao.org -g
+    yarn config set sass_binary_site http://cdn.npm.taobao.org/dist/node-sass -g
 fi;
 
 if ! [ -d "$HOME/Documents/ucmd" ]; then 
