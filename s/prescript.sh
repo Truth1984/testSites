9252b3693dad0a3b0ad8ee89b5a590fbc0f28113
awada <zhengyao365@163.com>
2020-07-29 16:41:10 +0800

    add docker auto config for current user

--- s/prescript.sh
+++ s/prescript.sh
@@ -122,4 +122,6 @@ if  ! [ -x "$(command -v docker)" ]; then
     echo docker compose
     curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
+
+    sudo usermod -aG docker "$USER" && sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 fi;
