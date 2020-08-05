6bbc28cd326c0bf2278d7a25200861f6d5a0326e
awada <zhengyao365@163.com>
2020-08-05 15:03:43 +0800

    add sudo for dc

--- s/prescript.sh
+++ s/prescript.sh
@@ -124,6 +124,6 @@ if  ! [ -x "$(command -v docker)" ]; then
 fi;
 
 if ! [ -x "$(command -v docker-compose)" ]; then
-    curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 fi;
