a6bd1116542082a434ae0779d59baa1cbb725c19
awada <awada@localhost.localdomain>
2020-08-13 09:06:14 +0800

    link docker compose

--- s/prescript.sh
+++ s/prescript.sh
@@ -167,4 +167,5 @@ fi;
 if ! [ -x "$(command -v docker-compose)" ]; then
     sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 fi;
