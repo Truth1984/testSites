fba48595afeaf357fd79bfd0cd3a2b084b6450c0
awada <awada@localhost.localdomain>
2020-08-17 13:14:49 +0800

    add firacode and vlc

--- s/desktop.sh
+++ s/desktop.sh
@@ -10,10 +10,22 @@ if $_gui; then
         sudo apt-get install -y snapd samba install ibus-libpinyin
     fi;
 
+    if ! [ -d $HOME/.local/share/fonts ]; then
+        mkdir -p $HOME/.local/share/fonts
+        for type in Bold Light Medium Regular Retina; do
+            file_path="${HOME}/.local/share/fonts/FiraCode-${type}.ttf"
+            file_url="https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true"
+            echo "wget -O $file_path $file_url"
+            wget -O "${file_path}" "${file_url}"
+        done
+        fc-cache -f
+    fi;
+
     if [ -x "$(command -v snap)" ]; then 
         # https://snapcraft.io/store
         sudo snap install postman
         sudo snap install code --classic
         sudo snap install mysql-workbench-community
+        sudo snap install vlc
     fi;
 fi;
