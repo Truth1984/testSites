93a441f27c048c7bef8f1f6898298038ecb07990
awada <awada@localhost.localdomain>
2020-12-02 16:10:48 +0800

    remove redundant

--- s/tools.sh
+++ s/tools.sh
@@ -3,14 +3,6 @@
 
 common="git nano nload e2fsprogs psmisc net-tools nethogs openssh-server iptables"
 
-if ! [ -x "$(command -v sudo)" ]; then
-    apt-get update -y
-    yum update -y
-
-    apt-get install -y sudo
-    yum install -y sudo 
-fi;
-
 if [ -x "$(command -v apt)" ]; then
     sudo apt-get install software-properties-common
     sudo apt-get install -y $common cron openssh-client
@@ -19,36 +11,6 @@ fi;
 if [ -x "$(command -v yum)" ]; then
     sudo yum install -y epel-release
     sudo yum install -y $common cronie openssh-clients
-fi;
-
-if ! [ -x "$(command -v n)" ]; then
-    common="curl screen npm"
-
-    sudo apt-get install -y software-properties-common 
-    sudo yum install -y epel-release 
-
-    sudo apt-get install -y $common
-    sudo yum install -y $common
-    
-    npm config set prefix $HOME/.npm_global
-
-    mkdir -p $HOME/.application/tmp
-    cd $HOME/.application/tmp
-    npm i n yarn
-    sudo $HOME/.application/tmp/node_modules/n/bin/n latest
-    PATH="$PATH"   
-    
-    $HOME/.application/tmp/node_modules/yarn/bin/yarn config set registry https://registry.npm.taobao.org -g
-    $HOME/.application/tmp/node_modules/yarn/bin/yarn config set prefix $HOME/.npm_global
-    $HOME/.application/tmp/node_modules/yarn/bin/yarn config set sass_binary_site http://cdn.npm.taobao.org/dist/node-sass -g
-    $HOME/.application/tmp/node_modules/yarn/bin/yarn global add n yarn 
-    source $HOME/.bashrc 
-    PATH="$PATH"
-  
-    cd $HOME && sudo rm -rf $HOME/.application/tmp   
-    
-    yarn global add nodemon typescript trash-cli
-    source $HOME/.bashrc
 fi;
 
 if ! [ -d "$HOME/Documents/ucmd" ]; then 
