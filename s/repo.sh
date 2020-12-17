1e1089804e5bbbf8b263d789553cc14de26aaa99
awada <zhengyao365@163.com>
2020-12-17 15:52:37 +0800

    change grep

--- s/repo.sh
+++ s/repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # bash <(curl -s https://truth1984.github.io/testSites/s/repo.sh)
 
-if grep -q ubuntu /etc/os-release ; then 
+if grep -q "ID=ubuntu" /etc/os-release ; then 
     codename=$(env -i bash -c '. /etc/os-release; echo $VERSION_CODENAME')
     echo -e "
     deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ $codename main restricted universe multiverse
@@ -15,7 +15,7 @@ if grep -q ubuntu /etc/os-release ; then
     " > /etc/apt/sources.list
 fi;
 
-if grep -q debian /etc/os-release ; then
+if grep -q "ID=debian" /etc/os-release ; then
     codename=$(dpkg --status tzdata|grep Provides|cut -f2 -d'-')
     echo -e "
     deb https://mirrors.tuna.tsinghua.edu.cn/debian/ $codename main contrib non-free
