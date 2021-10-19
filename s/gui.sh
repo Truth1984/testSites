0533217fa3630406e74beb3135d575ed2c27d97f
truth1984 <zhengyao365@163.com>
2021-10-19 10:56:34 +0800

    gui add os check

--- s/gui.sh
+++ s/gui.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-sudo yum groupinstall "Server with GUI" -y
-sudo yum install tigervnc-server xrdp -y
-sudo systemctl start xrdp.service
-sudo systemctl enable xrdp.service
+if $(u os rhel); then  
+    sudo yum groupinstall "Server with GUI" -y
+    sudo yum install tigervnc-server xrdp -y
+    sudo systemctl start xrdp.service
+    sudo systemctl enable xrdp.service
+fi;
 
+if $(u os ubuntu); then 
+    sudo apt-get install ubuntu-desktop
+    u install xrdp
+    u service -e=xrdp
+fi;
 # port: 3389
