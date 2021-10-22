58277733922f936b7fa62e809462e7a2e6cc43e4
truth1984 <zhengyao365@163.com>
2021-10-22 11:32:51 +0800

    ssh setup

---
+++ s/win/ssh-setup.ps1
@@ -0,0 +1,5 @@
+# https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
+
+# setup bash, require git bash installed
+
+echo cmd /k  "C:\Program Files\Git\bin\bash.exe" > bash.cmd
