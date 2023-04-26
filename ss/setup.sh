d4381a339fa1fadd9d099217f1b6fd7e3a1655b9
truth1984 <zhengyao365@163.com>
2023-04-26 16:15:30 +0800

    change name

--- ss/setup.sh
+++ ss/setup.sh
@@ -6,7 +6,7 @@
 
 ssurl="https://raw.gitmirror.com/Truth1984/shell-simple/main/util.sh"; if $(command -v curl &> /dev/null); then curl $ssurl -o util.sh; elif $(command -v wget &> /dev/null); then wget -O util.sh $ssurl; fi; chmod 777 util.sh && ./util.sh setup
 
-if [[ -z "$(command -v $1)" ]]; then echo u2 not setup correctly && exit 1; fi;
+if [[ -z "$(command -v u2)" ]]; then echo u2 not setup correctly && exit 1; fi;
 
 if $(u2 osCheck alpine); then
     sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
